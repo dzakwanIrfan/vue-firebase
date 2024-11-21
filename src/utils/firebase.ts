4d2d436fbc136ff44d5002455b6c8813a5491775
Dzakwan Irfan <lexdani368@gmail.com>
2024-11-21 07:01:46 +0700

    feat: add authentication with Google and implement user profile management

---
+++ src/utils/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEy1YnI8dXPqhKLxDuYTXon5N6z4Z2eAM",
+    authDomain: "vue-project-72067.firebaseapp.com",
+    projectId: "vue-project-72067",
+    storageBucket: "vue-project-72067.firebasestorage.app",
+    messagingSenderId: "411443132443",
+    appId: "1:411443132443:web:4ee4528e7eca9fbd9420be"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
